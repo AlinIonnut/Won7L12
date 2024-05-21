fb57a67c81e62bd6fa317ca61b8e2a7d2b4996b8
Scarlat Alin <scarlat.alin30@gmail.com>
2024-05-21 22:42:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("~List of integers~");
+Won7L12.Queue<int>  intQueue = new Won7L12.Queue<int> (6);
+intQueue.Enqueue (1);
+intQueue.Enqueue (2);
+intQueue.Enqueue (3);
+intQueue.Enqueue (4);
+Console.WriteLine ("Count: " + intQueue.Count);
+Console.WriteLine ("Dequeued: " + intQueue.Dequeue ());
+Console.WriteLine("Count after dequeue: " + intQueue.Count);
+
+
+Console.WriteLine("~List of objects~");
+Queue<string> stringQueue = new Queue<string>(3);
+stringQueue.Enqueue("first");
+stringQueue.Enqueue("second");
+stringQueue.Enqueue("third");
+
+Console.WriteLine("Count: " + stringQueue.Count);
+Console.WriteLine("Dequeued: " + stringQueue.Dequeue());
+Console.WriteLine("Dequeued: " + stringQueue.Dequeue());
+Console.WriteLine("Dequeued: " + stringQueue.Dequeue());
+Console.WriteLine("Dequeued: " + stringQueue.Dequeue());
+Console.WriteLine("Count after dequeue: " + stringQueue.Count);
---
+++ Queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Won7L12
+{
+    public class Queue<T>
+    {
+        private List<T> list;
+        private int capacityOfQueue;
+
+        public int Count { get { return list.Count; } }
+
+        public Queue(int capacity)
+        {
+            if(capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be bigger than zero!");
+            }
+
+            list = new List<T>();
+            capacityOfQueue = capacity;
+        }
+
+        public void Enqueue(T item)
+        {
+            if(list.Count >= capacityOfQueue)
+            {
+                throw new ArgumentException("Queue is full");
+            }
+
+            list.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("Queue is empty");
+            }
+
+            T item = list[0];
+            list.RemoveAt(0);
+            return item;
+        }
+    }
+}
